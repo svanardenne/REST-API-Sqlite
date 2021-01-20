791dd76d81440d4c3d3104c52a6af79330b8ada6
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-01-19 16:35:39 -0800

    Added validation for Users

--- models/User.js
+++ models/User.js
@@ -6,15 +6,46 @@ module.exports = (sequelize) => {
   User.init({
     firstName: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'First name cannot be empty'
+        }
+      }
     },
     lastName: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Last name cannot be empty'
+        }
+      }
     },
     emailAddress: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Email name cannot be empty'
+        },
+        isEmail: {
+          msg: 'Email is not valid'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Password name cannot be empty'
+        },
+        len: {
+          args: [8,20],
+          msg: 'Password must be between 8 and 20 characters long'
+        }
+      }
     }
   },
   {sequelize});
